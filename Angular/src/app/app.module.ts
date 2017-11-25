f7f5ca186ba57b4a61fe71137f367fe2f45af65e
saddamhusaain <saddamhusaainwins@gmail.com>
2017-11-25 18:40:25 +0530

    Setup angular project.

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,18 +1,40 @@
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms';
+import { AppRouting } from './shared/routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 
+// Common header & footer component
+import { HeaderComponent } from './shared/layout/header.component';
+import { FooterComponent } from './shared/layout/footer.component';
+
+// Custom components
+import { HomeComponent } from './home/home.component';
+import { AnnouncementComponent } from './announcement/announcement.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+
+        // Header & Footer component
+        FooterComponent,
+        HeaderComponent,
+
+        // Custom components
+        HomeComponent,
+        AnnouncementComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule,
+        AppRouting
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Angular/src/app/shared/routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+import { Component, ModuleWithProviders } from '@angular/core';
+
+
+// Compontes used in route.
+import { HomeComponent } from '../../home/home.component';
+import { AnnouncementComponent } from '../../announcement/announcement.component';
+
+/*
+  Created By   : Saddam
+  Created Date : 25-11-2017
+  Purpose      : Defining route with their components.
+*/
+const AppRoutes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'announcement', component: AnnouncementComponent },
+];
+
+
+export const AppRouting: ModuleWithProviders = RouterModule.forRoot(AppRoutes); 
